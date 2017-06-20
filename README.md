24e724da1ddb9eaffe0aca42eba1aefaccc0fa33
Sean Moran <sean@donking.synpromics.com>
2017-06-20 16:12:53 +0100

    Updates

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # emacs_init.el
 Configuration file for my personal emacs programming environment
 
-This Emacs environment configuration file enables a rich Python programming IDE. I tend to favoura lightweight and fast Emacs environment in my day-to-day programming work (rather than a more feature rich IDE such as PyCharm).
+This Emacs environment configuration file enables a rich Python programming IDE. I tend to favour a lightweight and fast Emacs environment in my day-to-day programming and research work (rather than a more feature rich IDE such as PyCharm).
 I've setup my Emacs environment according to the init.el file featured in this repository. The following packages are used:
 
 * Emacs material theme: https://github.com/cpaulik/emacs-material-theme
@@ -10,20 +10,24 @@ I've setup my Emacs environment according to the init.el file featured in this r
 * Pylint: for code analysis (coding standards, error detection) https://www.pylint.org/
 * Ropemacs: python refactorings and code-assists https://github.com/python-rope/ropemacs
 * Pymacs: allows both-way communication between Emacs Lisp and Python https://github.com/pinard/Pymacs
+* IPython: interactive code execution from within Emacs http://ipython.org/
+* Interactively Do Things (ido): makes switching between buffers, opening/closing files easy https://www.emacswiki.org/emacs/InteractivelyDoThings
+* autopep8: automatically formats Python code to conform to the [PEP 8](https://www.python.org/dev/peps/pep-0008/) style guide
+* sphinx-doc.el: generates [Sphinx](http://www.sphinx-doc.org/en/stable/) friendly docstrings https://github.com/naiquevin/sphinx-doc.el
 
 My configuration is largely built on the excellent blog article from Jess Hamrick: http://www.jesshamrick.com/2012/09/18/emacs-as-a-python-ide, with changes to the configuration here and there to adapt it to my own tastes and requirements. 
 
 To use this configuration in your own work, simply copy this file into ~/.emacs.d/. Then create the following directories with associated libraries within the ~/.emacs.d/ folder:
 
 * fill-column-indicator-1.83/  
-  * install https://github.com/alpaker/Fill-Column-Indicator
+  * install https://github.com/alpaker/Fill-Column-Indicator here
 * python-mode-6.0.11/ 
-  * install https://launchpad.net/python-mode
+  * install https://launchpad.net/python-mode here
 * ropemacs-0.7/ 
-  * install https://github.com/python-rope/ropemacs
+  * install https://github.com/python-rope/ropemacs here
 * pymacs-0.25/ 
-  * install https://github.com/pinard/Pymacs
+  * install https://github.com/pinard/Pymacs here
 * highlight-current-line-0.57/ 
-  * https://www.emacswiki.org/emacs/highlight-current-line.el
+  * https://www.emacswiki.org/emacs/highlight-current-line.el here
 
-This configuration has been tested on Centos/Redhat. Any feedback welcome!
+This configuration has currently been tested on Centos/Redhat. Any feedback on it welcome!
--- init.el
+++ init.el
@@ -1,7 +1,11 @@
 ;; init.el --- Emacs configuration
 
-;; INSTALL PACKAGES
-;; --------------------------------------
+; --------------------------------------;
+;; INSTALL PACKAGES                    ;;
+;; -------------------------------------;
+
+(require 'ido)
+(ido-mode t)
 
 (require 'package)
 
@@ -32,15 +36,17 @@
       (package-install package)))
       myPackages)
 
-;; BASIC CUSTOMIZATION
-;; --------------------------------------
+; --------------------------------------;
+;; BASIC CUSTOMIZATION                 ;;
+;; -------------------------------------;
 
 (setq inhibit-startup-message t) ;; hide the startup message
 (load-theme 'material t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
 
-;; PYTHON CONFIGURATION
-;; --------------------------------------
+; ---------------------------------------;
+;; PYTHON CONFIGURATION                ;;;
+;; --------------------------------------;
 
 (elpy-enable)
 (elpy-use-ipython)
@@ -49,6 +55,138 @@
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+(add-to-list 'load-path "~/.emacs.d/fill-column-indicator-1.83")
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+ global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
+; python-mode
+(setq py-install-directory "~/.emacs.d/python-mode-6.0.11")
+(add-to-list 'load-path py-install-directory)
+(require 'python-mode)
+
+; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+;------------;
+;;; Cursor ;;;
+;------------;
+
+; highlight the current line
+(add-to-list 'load-path "~/.emacs.d/highlight-current-line-0.57")
+(require 'highlight-current-line)
+(global-hl-line-mode t)
+(setq highlight-current-line-globally t)
+(setq highlight-current-line-high-faces nil)
+(setq highlight-current-line-whole-line nil)
+(setq hl-line-face (quote highlight))
+
+; don't blink the cursor
+(blink-cursor-mode nil)
+
+; make sure transient mark mode is enabled (it should be by default,
+; but just in case)
+(transient-mark-mode t)
+
+; turn on mouse wheel support for scrolling
+(require 'mwheel)
+(mouse-wheel-mode t)
+
+; ignore case when searching
+(setq case-fold-search t)
+
+; require final newlines in files when they are saved
+(setq require-final-newline t)
+
+;----------------------;
+;;; Windows & Frames ;;;
+;----------------------;
+
+; language
+(setq current-language-environment "English")
+
+; don't show the startup screen
+(setq inhibit-startup-screen t)
+; don't show the menu bar
+(menu-bar-mode nil)
+; don't show the tool bar
+(require 'tool-bar)
+(tool-bar-mode nil)
+; don't show the scroll bar
+(scroll-bar-mode nil)
+
+; number of characters until the fill column
+(setq fill-column 70)
+
+; specify the fringe width for windows -- this sets both the left and
+; right fringes to 10
+(require 'fringe)
+(fringe-mode 10)
+
+; lines which are exactly as wide as the window (not counting the
+; final newline character) are not continued. Instead, when point is
+; at the end of the line, the cursor appears in the right fringe.
+(setq overflow-newline-into-fringe t)
+
+; each line of text gets one line on the screen (i.e., text will run
+; off the left instead of wrapping around onto a new line)
+(setq truncate-lines t)
+; truncate lines even in partial-width windows
+(setq truncate-partial-width-windows t)
+
+; display line numbers to the right of the window
+(global-linum-mode t)
+; show the current line and column numbers in the stats bar as well
+(line-number-mode t)
+(column-number-mode t)
+
+;-------------------------;
+;;; Syntax Highlighting ;;;
+;-------------------------;
+
+; text decoration
+(require 'font-lock)
+(setq font-lock-maximum-decoration t)
+(global-font-lock-mode t)
+(global-hi-lock-mode nil)
+(setq jit-lock-contextually t)
+(setq jit-lock-stealth-verbose t)
+
+; if there is size information associated with text, change the text
+; size to reflect it
+(size-indication-mode t)
+
+; highlight parentheses when the cursor is next to them
+(require 'paren)
+(show-paren-mode t)
+
+; pymacs
+(add-to-list 'load-path "~/.emacs.d/pymacs-0.25")
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+
+; ropemacs
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
 
 ;; enable autopep8 formatting on save
 (require 'py-autopep8)
